5ebd218edf1aa440eb44ed0c522e4777e82ed74a
Bella Belgarokova <bella.belgarokova@hotmail.com>
2020-11-11 18:29:09 +0300

    add table display functionality

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,10 @@ public class Main {
         int y = 1;
         while (x <= 10) {
             while (y <= 10) {
-                System.out.println(x + " x " + y + " = " + (x * y));
+                System.out.print(y + " x " + x + " = " + (x * y) + "\t\t");
                 y++;
             }
+            System.out.println();
             x++;
             y = 1;
         }
