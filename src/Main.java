2298cb36936303f29874170126480c67baf214b9
Bella Belgarokova <bella.belgarokova@hotmail.com>
2020-11-11 17:10:24 +0300

    adjust limit to include 10

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ public class Main {
     public static void main(String[] args) {
         int x = 1;
         int y = 1;
-        while (x < 10) {
-            while (y < 10) {
+        while (x <= 10) {
+            while (y <= 10) {
                 System.out.println(x + " x " + y + " = " + (x * y));
                 y++;
             }
